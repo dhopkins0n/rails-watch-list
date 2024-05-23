bd76b7300e9d0b98ec10121a0c35f215b1137291
david hopkinson <hopkinson.dave@yahoo.co.uk>
2024-05-23 15:58:29 +0100

    logic complete

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,44 @@
+class ListsController < ApplicationController
+
+
+  before_action :set_list, only: %i[show]
+def new
+  @list = List.new
+end
+
+def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: "list was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+end
+
+
+  def show
+  end
+
+  def index
+    @lists = List.all
+  end
+
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :lists, only: [:new, :create, :show, :index] do
+    resources :bookmarks, only: [:new, :create]
+  end
+resources :bookmarks, only: [:destroy]
+
 end
+
+
+# GET "lists"
+# A user can see the details of a given list and its name
+# GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
